003e287ab9c8e787b287b231793fe56c5814f37b
Mohammad Moradi <mmoradi.hu@gmail.com>
2014-09-17 08:14:57 +0430

    modified:   alert.py

--- alert.py
+++ alert.py
@@ -223,9 +223,9 @@ def send_alert():
 	mailserver = smtplib.SMTP('smtp.gmail.com',587)
 	# identify ourselves to smtp gmail client
 	mailserver.ehlo()
-	# secure our email with tls encryption
+	# secure email with tls encryption
 	mailserver.starttls()
-	# re-identify ourselves as an encrypted connection
+	# re-identify as an encrypted connection
 	mailserver.ehlo()
 	mailserver.login('tswsystem@gmail.com', 'tsunami@me')
 	mailserver.sendmail('',receiver ,msg.as_string())
